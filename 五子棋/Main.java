398d26e7f558c8fd20645e0b789ecf76b4ae7b6c
xuzuomu <email1805625678@qq.com>
2020-02-10 21:03:09 +0800

    五子棋代码“
    git remote add origin https://github.com/xuzuomu/Gogang.git
    git pull origin master
    git push -u origin master
    git commit -m五子棋代码

---
+++ 五子棋/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package 五子棋;
+
+/**
+ *
+ * @author 李春
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new Windows("五子棋");
+    }
+}
